0cda0582fe95c4fbf2171f1ddd0885ed40e9abc4
akhileshsingh1111 <akhilesh.singh1111@yahoo.in>
2017-09-23 17:57:43 +0530

    Add files via upload

---
+++ routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router'
+import { ProjectsListComponent } from './projects/projects-list.component'
+import { ProjectDetailsComponent } from './projects/project-details/project-details.component'
+import { CreateSessionComponent } from './projects/project-details/create-session.component'
+import { CreateProjectComponent } from './projects/create-project.component'
+import {ProjectServiceResolver} from './projects/projectservice-resolver'
+import { Error404Component } from './errors/404.component'
+export const appRoutes: Routes = [
+    { path: 'projects/new', component: CreateProjectComponent },
+    { path: 'projects', component: ProjectsListComponent },
+    //{ path: 'projects', component: ProjectsListComponent, resolve:{projects:ProjectServiceResolver} },
+    { path: 'projects/:id', component: ProjectDetailsComponent },
+    { path: 'projects/session/new', component: CreateSessionComponent },
+    { path: '', redirectTo: '/projects', pathMatch: 'full' },
+    { path: '404', component: Error404Component },
+    { path:'user',  loadChildren:'app/user/user.module#UserModule'},
+]
